d82c9f7dc7204a7dc5ca2e92ba243375dccd13f1
KAST13L <o4enbsmeshno@gmail.com>
2022-09-22 15:00:00 +0300

    made useAppSelector hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,12 @@ import {HashRouter} from 'react-router-dom';
 import {Header} from "../features/Header/Header";
 import {Pages} from "../features/Header/Pages";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "./redux/store";
-import {RequestStatusType} from "./redux/app-reducer";
+import {useAppSelector} from "./redux/store";
 import {LinearProgress} from "@material-ui/core";
 
 export const App = () => {
 
-    const status = useSelector<AppRootStateType, RequestStatusType>( state => state.app.status)
+    const status = useAppSelector( state => state.app.status)
 
     return (
         <div>
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -2,6 +2,7 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk, {ThunkAction} from "redux-thunk";
 import {AppActionsType, appReducer} from "./app-reducer";
 import {AuthActionsType, authReducer} from "./auth-reducer";
+import {TypedUseSelectorHook, useSelector} from "react-redux";
 
 const rootReducer = combineReducers({
     app: appReducer,
@@ -13,8 +14,9 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 
 
 export type RootActionsType = AppActionsType | AuthActionsType
-
 export type ThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, RootActionsType>
+
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // @ts-ignore
 window.store = store;
